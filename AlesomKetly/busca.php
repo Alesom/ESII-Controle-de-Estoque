fa371bdaad355320538f97fe205cfe8aa001059c
Alesom <alesom.zorzi@gmail.com>
2016-08-16 15:44:51 -0300

    Alterações Iniciais

---
+++ AlesomKetly/busca.php
@@ -0,0 +1,23 @@
+<?php
+	require_once 'inserts/functions.php';
+	require_once 'inserts/logindb.php';
+
+	$banco = conectadb($dbHostname, $dbUsername, $dbPassword);
+	selectdb($banco, $dbDatabase);
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>busca</title>
+		<meta charset="UTF-8">
+
+		<link rel="stylesheet" type="text/css" href="css/generalStyles.css">
+		<link href="css/bootstrap.css" rel="stylesheet">
+	</head>
+	<body>
+		0
+	</body>
+</html>
+
+?>
