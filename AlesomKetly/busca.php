b6326a404561f7fa052bf2dcb2301627ea9341b3
Alesom <alesom.zorzi@gmail.com>
2016-08-23 15:42:21 -0300

    Busca funcionando para todos os campos - Fase de testes

--- AlesomKetly/busca.php
+++ AlesomKetly/busca.php
@@ -13,24 +13,39 @@
 		<script type="text/javascript">
 
 			function preencheBusca() {
-				var valor = document.getElementById("search").value;
-				var url = 'load.php?valor=' + valor;
+				var codp = document.getElementById("IdCodP").value;
+				var nomep = document.getElementById("IdNomeP").value;
+				var codg = document.getElementById("IdCodG").value;
+				var nomeg = document.getElementById("IdNomeG").value;
+				var codl = document.getElementById("IdCodL").value;
+				var nomel = document.getElementById("IdNomeL").value;
+				var url = 'load.php?codp=' + codp + '&nomep=' + nomep + '&codg=' + codg + '&nomeg=' + nomeg + 
+							'&codl=' + codl + '&nomel=' + nomel;
 				$.get(url, function(dataReturn) {
 					$('#idpd').html(dataReturn);
 				});
 			};
 		</script>
 	</head>
-	<body>
-
-		<br><br>
+	<body onload="preencheBusca()">
 
 		<div class="">
 		
 			<div class="">
 	      		<form name="fsearch" action="" method="POST">
     				<div class="">
-      					<input id="search" type="search" name="textsd" oninput="preencheBusca()" required>
+      					<input id="IdCodP" type="search" name="codp" 
+      						placeholder="Código do Produto" oninput="preencheBusca()"> <br/>
+      					<input id="IdNomeP" type="search" name="nomep" 
+      						placeholder="Nome do Produto" oninput="preencheBusca()"> <br/>
+      					<input id="IdCodG" type="search" name="codg" 
+      						placeholder="Código do Grupo" oninput="preencheBusca()"> <br/>
+      					<input id="IdNomeG" type="search" name="nomeg" 
+      						placeholder="Nome do Grupo" oninput="preencheBusca()"> <br/>
+      					<input id="IdCodL" type="search" name="codl" 
+      						placeholder="Código do Local" oninput="preencheBusca()"> <br/>
+      					<input id="IdNomeL" type="search" name="nomel" 
+      						placeholder="Nome do Local" oninput="preencheBusca()"> <br/>
       					<button type="submit" class="hide" disabled></button>
     				</div>
   				</form>
@@ -38,7 +53,7 @@
 
 			<div class="">
 				<div class="" id="idpd">
-				
+
 				</div>
 			</div>
 
--- AlesomKetly/load.php
+++ AlesomKetly/load.php
@@ -1,5 +1,10 @@
 <?php
-	$pesquisa = $_GET['valor'];
+	$codp = $_GET['codp'];
+	$nomep = $_GET['nomep'];
+	$codg = $_GET['codg'];
+	$nomeg = $_GET['nomeg'];
+	$codl = $_GET['codl'];
+	$nomel = $_GET['nomel'];
 
 	require_once 'inserts/functions.php';
 	require_once 'inserts/logindb.php';
@@ -10,7 +15,9 @@
 	
 	$query = "SELECT p.cod AS codp, p.nome AS nomep, p.qtd AS qtd, g.nome AS nomeg, l.nome AS nomel 
 				FROM produto AS p INNER JOIN grupo AS g on (p.codg = g.codg) INNER JOIN local AS l on (p.codl = l.codl)
-				WHERE p.cod LIKE '%" . $pesquisa . "%' ORDER BY p.cod";
+				WHERE p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%' 
+				AND g.nome LIKE '%" . $nomeg . "%' AND l.codl LIKE '%" . $codl . "%' AND l.nome LIKE '%" . $nomel . "%'
+				ORDER BY p.cod";
 
 	$resultado = query($banco, $query);
 	$num = mysqli_num_rows($resultado);
