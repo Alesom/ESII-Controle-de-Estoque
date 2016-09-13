244328eb087b29fa25891c262fae1e67602b6e36
Alesom <alesom.zorzi@gmail.com>
2016-09-13 16:43:06 -0300

    Separando as consultas em duas para depois ordenar

--- consultRelatorio.php
+++ consultRelatorio.php
@@ -10,25 +10,17 @@
 	$ano = $_GET['ano'];
 	$saida = $_GET['saida'];
 	$entrada = $_GET['entrada'];
-
-#	$query = "SELECT p.cod AS codp, p.nome AS nomep, p.qtd AS qtd, g.nome AS nomeg, l.nome AS nomel 
-#				FROM produto AS p INNER JOIN grupo AS g on (p.codg = g.codg) INNER JOIN local AS l on (p.codl = l.codl)
-#				WHERE p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%' 
-#				AND g.nome LIKE '%" . $nomeg . "%' AND l.codl LIKE '%" . $codl . "%' AND l.nome LIKE '%" . $nomel . "%'
-#				ORDER BY p.cod";
-
-	$query = "SELECT p.cod AS codp, p.nome AS nomep, p.qtd AS qtd, g.nome 
-							AS nomeg, l.nome AS nomel, s.data AS datas, e.data AS datae, e.codp 
-							AS ecodp, s.codp AS scodp 
-				FROM insercao AS e CROSS JOIN remocao AS s
-				INNER JOIN produto AS p on (p.cod = s.codp or p.cod =e.codp)
-				INNER JOIN grupo AS g on (p.codg = g.codg) 
-				INNER JOIN local AS l on (p.codl = l.codl)
-				WHERE p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%' 
+	
+	$Saida = "SELECT p.cod AS codp, p.nome AS nomep, s.qtd AS qtd, g.nome 
+							AS nomeg, l.nome AS nomel, s.data AS datas, s.codp AS scodp 
+							FROM produto AS p INNER JOIN remocao AS s on (p.cod = s.codp)
+							INNER JOIN grupo AS g on (p.codg = g.codg) 
+							INNER JOIN local AS l on (p.codl = l.codl)
+							WHERE p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%' 
 				AND g.nome LIKE '%" . $nomeg . "%' AND l.codl LIKE '%" . $codl . "%' AND l.nome LIKE '%" . $nomel . "%'
-				ORDER BY e.data, s.data";
+				ORDER BY s.data";
 
-	$resultado = query($conexao, $query);
+	$resultado = query($conexao, $Saida);
 	$num = mysqli_num_rows($resultado);
 
 	if ($num>0) {
@@ -37,8 +29,34 @@
 			echo "<td><center>". $row['codp'] . "</center></td> <td>". $row['nomep'] . "</td> 
 			<td><center>" . $row['qtd'] . "</center></td> <td><center>" . $row['nomeg'] . "</center></td> 
 			<td><center>" . $row['nomel']."</center></td> 
-			<td><center>" . $row['datae']."</center></td> 
+			<td><center>---</center></td> 
 			<td><center>" . $row['datas'];
+			echo "</tr> <br/>";
+		}
+	} else {
+		echo '<p>Nenhum produto encontrado.</p>';
+	}
+
+	$Entrada = "SELECT p.cod AS codp, p.nome AS nomep, e.qtd AS qtd, g.nome 
+							AS nomeg, l.nome AS nomel, e.data AS datae, e.codp AS ecodp 
+							FROM produto AS p INNER JOIN insercao AS e on (p.cod = e.codp)
+							INNER JOIN grupo AS g on (p.codg = g.codg) 
+							INNER JOIN local AS l on (p.codl = l.codl)
+							WHERE p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%' 
+				AND g.nome LIKE '%" . $nomeg . "%' AND l.codl LIKE '%" . $codl . "%' AND l.nome LIKE '%" . $nomel . "%'
+				ORDER BY e.data";
+
+	$resultado = query($conexao, $Entrada);
+	$num = mysqli_num_rows($resultado);
+
+	if ($num>0) {
+		while ($row = mysqli_fetch_assoc($resultado)) {
+			echo "<tr>";
+			echo "<td><center>". $row['codp'] . "</center></td> <td>". $row['nomep'] . "</td> 
+			<td><center>" . $row['qtd'] . "</center></td> <td><center>" . $row['nomeg'] . "</center></td> 
+			<td><center>" . $row['nomel']."</center></td> 
+			<td><center>". $row['datae']."</center></td> 
+			<td><center>---" ;
 			echo "</tr> <br/>";
 		}
 	} else {
