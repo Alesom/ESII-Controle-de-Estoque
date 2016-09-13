78ff67bd5a38cdd0650d9ae5726019b9ee7e9c5a
Andreluiz04 <andre.luiz_1985@hotmail.com>
2016-09-13 14:54:37 -0300

    relatorios

--- grafico.php
+++ grafico.php
@@ -1,3 +1,4 @@
+
 <html>	
 	<head>	
 	<title>Gráfico</title>	
@@ -20,10 +21,34 @@
 		<a href="index.php?logout=1"><button>Logout</button></a>
 	</div>
 	<div align="center">
-		<fieldset>
-			<img src="imagem.php"/>
-		</fieldset>
+		<a href="imagem.php?type=barra"><button>Barra</button></a>
+		<a href="imagem.php?type=pizza"><button>Pizza</button></a>
+		<a href="imagem.php?type=bolha"><button>Bolha</button></a>
+		<a href="imagem.php?type=pontos"><button>Pontos</button></a>
 	</div>
+	
+	<form>
+		<input type= "number" name ="datai" value="2016" />
+		<input type = "radio" name = 'periodo' value = 'anual' checked /> anual<br/>
+		<select name="mes">
+			<option name="jan" > Janeiro</option>
+			<option name="fev" > Fevereiro</option>
+			<option name="mar" > Março</option>
+			<option name="abr" > Abril</option>
+			<option name="mai" > Maio</option>
+			<option name="jun" > Junho</option>
+			<option name="jul" > Julho</option>
+			<option name="ago" > Agosto</option>
+			<option name="set" > Setembro</option>
+			<option name="out" > Outubro</option>
+			<option name="nov" > Novembro</option>
+			<option name="dez" > Dezembro</option>
+		</select>
+		<input type = "radio" name = 'periodo' value = 'mensal' /> mensal<br/>		
+		<input type= "text" name ="datai" placeholder="Data Inicial" />
+		<input type= "text" name ="dataf" placeholder="Data Final" />
+		<input type = "radio" name = 'periodo' value = 'interval' /> intervalo		
+	</form>
 
 	</body>
 </html>
--- imagem.php
+++ imagem.php
@@ -3,13 +3,6 @@
 include('phplot/phplot.php');
 require('connect.php');
 #Matriz utilizada para gerar os graficos
-$data = array(
-array('Jan', 20, 2, 4), array('Fev', 30, 3, 4), array('Mar', 20, 4, 14),
-array('Abr', 30, 5, 4), array('Mai', 13, 6, 4), array('Jun', 37, 7, 24),
-array('Jul', 10, 8, 4), array('Ago', 15, 9, 4), array('Set', 20, 5, 12),
-array('Out', 28, 4, 14), array('Nov', 16, 7, 14), array('Dez', 24, 3, 15),
-);
-
 	$data1;
 	$a=0;
 	$sql = "SELECT * FROM produto";
@@ -31,7 +24,22 @@ $plot = new PHPlot(800,500);
 #Tipo de borda, consulte a documentacao
 $plot->SetImageBorderType('none');
 #Tipo de grafico, nesse caso barras, existem diversos(pizza…)
-$plot->SetPlotType('bars');
+if(isset($_GET["type"])){
+	switch ($_GET["type"]){
+	case 'barra':
+		$plot->SetPlotType('bars');
+		break;
+	case 'pizza':
+		$plot->SetPlotType('pie');
+		break;
+	case 'bolha':
+		$plot->SetPlotType('bubbles');
+		break;
+	case 'pontos':
+		$plot->SetPlotType('linepoints');
+		break;
+	}
+}	
 #Tipo de dados, nesse caso texto que esta no array
 $plot->SetDataType('text-data');
 #Setando os valores com os dados do array
