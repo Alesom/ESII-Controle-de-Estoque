b4eb724f396862ab5daec0577d6de70345d818ed
JoaoBarp <joaorbarp@hotmail.com>
2017-01-31 20:19:27 -0200

    Funcionando a principio
    Funcionando a principio

--- graficos.php
+++ graficos.php
@@ -4,29 +4,32 @@
   require('connect.php');
 
   $codp = $_GET['codp'];
-  $nomep = $_GET['nomep'];
-  $codg = $_GET['codg'];
-  $nomeg = $_GET['nomeg'];
-  $codl = $_GET['codl'];
-  $nomel = $_GET['nomel'];
-  $dataI = $_GET['diai'];
-  $dataF = $_GET['diaf'];
+	$nomep = $_GET['nomep'];
+	$codg = $_GET['codg'];
+	$nomeg = $_GET['nomeg'];
+	$codl = $_GET['codl'];
+	$nomel = $_GET['nomel'];
+	$dataI = $_GET['diai'];
+	$dataF = $_GET['diaf'];
+	$saidaF = $_GET['saida'];
+	$entradaF = $_GET['entrada'];
 
   $vetSAI;
   $vetEN;
-  $NSaidaRes;
-  $NEntradaRes;
+  $SaidaRes;
+  $EntradaRes;
 
   if($_GET['saida'] == 1){
-    $Saida = "SELECT p.nome AS nomep, sum(s.qtd) as soma
-              FROM produto AS p
+    $Saida = "SELECT p.nome AS nomep,p.cod as codp, sum(s.qtd) as soma
+							FROM produto AS p
               INNER JOIN remocao AS s on (p.cod = s.codp)
-              INNER JOIN grupo AS g on (p.codg = g.codg)
-              INNER JOIN local AS l on (p.codl = l.codl)
-              WHERE s.data between ('" . $dataI . "') AND ('" . $dataF . "') AND
-              p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%'
-              AND g.nome LIKE '%" . $nomeg . "%' AND l.codl LIKE '%" . $codl . "%' AND l.nome LIKE '%" . $nomel . "%'
-              GROUP BY p.nome";
+							INNER JOIN localizacao AS lz on (p.cod = lz.codp)
+							INNER JOIN grupo AS g on (SUBSTRING(p.cod,1,1) = g.codg)
+							INNER JOIN local AS l on (l.codl = lz.codl)
+							WHERE s.data between '$dataI' AND '$dataF' AND
+							p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%'
+				AND g.nome LIKE '%" . $nomeg . "%' AND l.codl LIKE '%" . $codl . "%' AND l.nome LIKE '%" . $nomel . "%'
+				GROUP BY p.nome";
     $i=0;
     $SaidaRes = query($conexao, $Saida) or die(mysql_error());
     $NSaidaRes = mysqli_num_rows($SaidaRes);
@@ -38,15 +41,16 @@
   }
 
   if ($_GET['entrada'] == 1){
-    $Entrada = "SELECT p.nome AS nomep, sum(s.qtd) as soma
-                FROM produto AS p
-                INNER JOIN insercao AS s on (p.cod = s.codp)
-                INNER JOIN grupo AS g on (p.codg = g.codg)
-                INNER JOIN local AS l on (p.codl = l.codl)
-                WHERE s.data between ('" . $dataI . "') AND ('" . $dataF . "') AND
-                p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%'
-                AND g.nome LIKE '%" . $nomeg . "%' AND l.codl LIKE '%" . $codl . "%' AND l.nome LIKE '%" . $nomel . "%'
-                GROUP BY p.nome";
+    $Entrada = "SELECT p.nome AS nomep,p.cod as codp, sum(e.qtd) as soma
+							FROM produto AS p
+              INNER JOIN insercao AS e on (p.cod = e.codp)
+							INNER JOIN localizacao AS lz on (p.cod = lz.codp)
+							INNER JOIN grupo AS g on (SUBSTRING(p.cod,1,1) = g.codg)
+							INNER JOIN local AS l on (l.codl = lz.codl)
+							WHERE e.data between '$dataI' AND '$dataF' AND
+							p.cod LIKE '%" . $codp . "%' AND p.nome LIKE '%" . $nomep . "%' AND g.codg LIKE '%" . $codg . "%'
+				AND g.nome LIKE '%" . $nomeg . "%' AND l.codl LIKE '%" . $codl . "%' AND l.nome LIKE '%" . $nomel . "%'
+				GROUP BY p.nome";
     $i=0;
     $EntradaRes = query($conexao, $Entrada) or die(mysql_error());
     $NEntradaRes = mysqli_num_rows($EntradaRes);
